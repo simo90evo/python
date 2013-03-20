1637558b5e18991f922b776d3a7d82908abb7766
Simone <simo90@me.com>
2013-03-20 15:08:48 +0100

    Create typeData.py
    type of data in python

---
+++ typeData.py
@@ -0,0 +1,23 @@
+# help(obj) --> return what obj do
+# type(obJ) --> return the type of bj
+# dir(obj)  --> return all the methods that you can apply to obj
+
+import math #import the library math, you can import every library that you want
+
+l = ["string", 23] # is a list, in it you can put all types
+S = set(["string", 23]) # is a set, in it you can put all types, you can do all set operations
+t = ("string", 23, math.log) # is a tuple, in it you can put all types, but you can't insert other values after the initialization
+d = {"name":value,"sq":math.sqrt} # is a dictionary (associative array)
+s = "hello" # is string
+n = 23 # is number
+n**2 # elevazionea potenza
+23/4
+23/4.0
+23//4.0
+0x esadecimale
+0ff ottale
+i=1j # complex
+
+
+# value in element --> return true or false it dipends if value is in element (works only with iterable)
+
